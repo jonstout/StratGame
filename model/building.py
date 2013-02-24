0b2df0a32a023b76bc32ff68ee7c458fe2c3ccc6
Jonathan Stout <jonstout@indiana.edu>
2013-02-23 21:58:51 -0500

    Fixed building string to include type and building_id

--- model/building.py
+++ model/building.py
@@ -86,6 +86,6 @@ class Building(object):
 		return self._units[utype] if utype in self._units else None
     
 	def __str__(self):
-		s = "bID: {}, Position: ({}, {}), ".format(self._type, self._x, self._y)
+		s = "bID: {}, Type: {}, Position: ({}, {}), ".format(self._id, self._type, self._x, self._y)
 		s += "pID: {}, HP: {}, Units: {}".format(self.player_id, self._hp, self._units)
 		return s
